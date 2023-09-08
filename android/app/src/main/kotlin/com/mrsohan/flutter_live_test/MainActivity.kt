ec51586289dc4b6e4455bd150901f9d4de0857d2
MrSohan10 <iamsohan11@gmail.com>
2023-09-08 23:32:10 +0600

    module 5 live test completed

---
+++ android/app/src/main/kotlin/com/mrsohan/flutter_live_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrsohan.flutter_live_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 50,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Jhon Doe',
+                  style: TextStyle(color: Colors.green, fontSize: 30)),
+              Text(
+                'Flutter Batch 4',
+                style: TextStyle(color: Colors.blue, fontSize: 25),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
