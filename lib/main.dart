da13f12a23cc7ceb80c8e16fa199ab89d2e308dd
MrSohan10 <iamsohan11@gmail.com>
2023-10-06 23:41:31 +0600

    live test module 9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,43 +11,152 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('News Feed'),
-        ),
-        body: const ImageFeed(),
-      ),
+      home: SizeSelector(),
     );
   }
 }
 
-class ImageFeed extends StatefulWidget {
-  const ImageFeed({super.key});
 
+
+class SizeSelector extends StatefulWidget {
   @override
-  ImageFeedState createState() => ImageFeedState();
+  _SizeSelectorState createState() => _SizeSelectorState();
 }
 
-class ImageFeedState extends State<ImageFeed> {
+class _SizeSelectorState extends State<SizeSelector> {
+  String selectedSize = 'S';
+
+  void _onSizeSelected(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected size: $size'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Size Selector'),
       ),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            elevation: 10,
-            child: Image.network('https://via.placeholder.com/150'),
-          ),
-        );
-      },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('S'),
+                      child: Text('S'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'S' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('M'),
+                      child: Text('M'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'M' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('L'),
+                      child: Text('L'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'L' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('XL'),
+                      child: Text('XL'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'XL' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 8, 8, 8),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('XXL'),
+                      child: Text('XXL'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'XXL' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () => _onSizeSelected('XXXL'),
+                      child: Text('XXXL'),
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                        selectedSize == 'XXXL' ? Colors.orange : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
