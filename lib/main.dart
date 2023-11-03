2e663158e597d6a1e3634e29de9254f4ad783d76
MrSohan10 <iamsohan11@gmail.com>
2023-11-03 23:36:37 +0600

    live test 11 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_live_test/screen/home_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.account_circle_rounded, size: 50, color: Colors.green),
-              Text('Jhon Doe',
-                  style: TextStyle(color: Colors.green, fontSize: 30)),
-              Text(
-                'Flutter Batch 4',
-                style: TextStyle(color: Colors.blue, fontSize: 25),
-              )
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: const HomePage(),
     );
   }
 }
+
+
---
+++ lib/screen/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> weatherInfo = [
+    {
+      "city": "New York",
+      "temperature": 20,
+      "condition": "Clear",
+      "humidity": 60,
+      "windSpeed": 5.5
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 25,
+      "condition": "Sunny",
+      "humidity": 50,
+      "windSpeed": 6.8
+    },
+    {
+      "city": "London",
+      "temperature": 15,
+      "condition": "Partly Cloudy",
+      "humidity": 70,
+      "windSpeed": 4.2
+    },
+    {
+      "city": "Tokyo",
+      "temperature": 28,
+      "condition": "Rainy",
+      "humidity": 75,
+      "windSpeed": 8.0
+    },
+    {
+      "city": "Sydney",
+      "temperature": 22,
+      "condition": "Cloudy",
+      "humidity": 55,
+      "windSpeed": 7.3
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Info App'),
+      ),
+      body: ListView.separated(
+        itemCount: weatherInfo.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('City: ${weatherInfo[index]["city"]}'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Temperature: ${weatherInfo[index]["temperature"].toString()}C'),
+                  Text('Condition: ${weatherInfo[index]["condition"].toString()}'),
+                  Text('Humidity: ${weatherInfo[index]["humidity"].toString()}%'),
+                  Text('Wind Speed ${weatherInfo[index]["windSpeed"].toString()} m/s'),
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (_, __) => Divider(),
+      ),
+    );
+  }
+}
